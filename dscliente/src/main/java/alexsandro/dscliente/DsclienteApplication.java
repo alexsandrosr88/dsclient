24572fa0b5a73e1966240fc74f3e1ebe37bf19e5
Alexsandro Ramos <alexsandrosr@msn.com>
2021-10-17 17:30:07 -0300

    Project created

---
+++ dscliente/src/main/java/alexsandro/dscliente/DsclienteApplication.java
@@ -0,0 +1,13 @@
+package alexsandro.dscliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsclienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsclienteApplication.class, args);
+	}
+
+}
---
+++ dscliente/src/test/java/alexsandro/dscliente/DsclienteApplicationTests.java
@@ -0,0 +1,13 @@
+package alexsandro.dscliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsclienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
