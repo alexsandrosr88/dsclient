9fbef32c2a6f9402c615a99e937c984ead3f5062
alexsandrosr88 <63133361+alexsandrosr88@users.noreply.github.com>
2021-10-18 02:25:47 -0300

    Update client

--- dsclient/src/main/java/alexsandro/dsclient/services/ClientService.java
+++ dsclient/src/main/java/alexsandro/dsclient/services/ClientService.java
@@ -2,13 +2,13 @@ package alexsandro.dsclient.services;
 
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import alexsandro.dsclient.dto.ClientDTO;
 import alexsandro.dsclient.entities.Client;
@@ -45,4 +45,20 @@ public class ClientService {
 		return new ClientDTO(entity);
 	}
 	
+	@Transactional
+	public ClientDTO update(Long id , ClientDTO dto) {
+		try {
+			Client entity = repository.getOne(id);
+			entity.setName(dto.getName());
+			entity.setCpf(dto.getCpf());
+			entity.setIncome(dto.getIncome());
+			entity.setBirthDate(dto.getBirthDate());
+			entity.setChildren(dto.getChildren());
+			entity = repository.save(entity);
+			return new ClientDTO(entity);
+		} 
+		catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException("ID: "+id+" not found");
+		}
+	}
 }
